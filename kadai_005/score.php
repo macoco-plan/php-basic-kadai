5729ab3c483e0773a9961c4947317ee879976724
macoco-plan <macoco.plan@gmail.com>
2024-10-28 10:48:03 +0900

    新規作成

---
+++ kadai_005/score.php
@@ -0,0 +1,23 @@
+<?php
+//　変数に平均点を代入する
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+//　変数に合計数を代入する
+$all_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+//　平均値を計算し代入する
+echo $all_score / 10;
+
+?>
+
+
+
